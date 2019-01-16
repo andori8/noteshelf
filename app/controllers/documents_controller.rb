a983bb3e23a39250ba34b3962a2a589b107b3595
Ryan <falcsmike7@gmail.com>
2019-01-15 20:19:22 -0500

    created form and show page

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,6 @@
 class DocumentsController < ApplicationController
+  before_action :find_document, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -6,9 +8,16 @@ class DocumentsController < ApplicationController
   end
 
   def new
+    @document = Document.new
   end
 
   def create
+    @document = Document.new(document_params)
+    if @document.save
+      redirect_to @document
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -21,10 +30,12 @@ class DocumentsController < ApplicationController
   end
 
   private
-  
+
   def find_document
+    @document = Document.find(params[:id])
   end
 
   def document_params
+    params.require(:document).permit(:title, :content)
   end
 end
